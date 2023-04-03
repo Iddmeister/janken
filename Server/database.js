c2eef0c6e8559fc49aa41a14859bdfd199e91237
Iddmeister <iddonagenda.t@gmail.com>
2023-04-03 15:03:40 +0100

    Database Fix, I don't know why it broke

--- Server/database.js
+++ Server/database.js
@@ -2,6 +2,7 @@ var mysql = require("mysql")
 
 var database = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: process.env.DBPASS,
     database: "janken",
